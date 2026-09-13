560b8f28f458c00280a545ee3a60a639a3e0a745
destroyer100-coder <vanshjawla504@gmail.com>
2026-09-13 14:15:03 +0530

    Use sessionStorage instead of localStorage for authentication to auto-logout on browser close

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,7 @@ const API = axios.create({
 })
 
 API.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem('taskflow_user') || '{}')
+  const user = JSON.parse(sessionStorage.getItem('taskflow_user') || '{}')
   if (user?.token) config.headers.Authorization = `Bearer ${user.token}`
   return config
 })
